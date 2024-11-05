e463e56d0deeef204476dc644ce43377a70e3855
Janavi Patil <128917596+JanaviPatil46@users.noreply.github.com>
2024-11-05 04:21:23 -0800

    05-11

--- invoice-backend/routes/invoiceRoutes.js
+++ invoice-backend/routes/invoiceRoutes.js
@@ -1,7 +1,26 @@
+// // const express = require('express')
+// // const router = express.Router()
+
+// // const { getInvoices, getInvoice, createInvoice, deleteInvoice, updateInvoice, getInvoiceList, getInvoiceListbyid } = require('../controller/invoiceController')
+
+
+
+// // router.get('/invoice', getInvoices)
+// // router.get('/invoice/:id', getInvoice)
+// // router.post('/invoice', createInvoice)
+// // router.delete('/invoice/:id', deleteInvoice)
+// // router.patch('/invoice/:id', updateInvoice)
+// // router.get('/invoice/invoicelist', getInvoiceList)
+// // router.get('/invoice/invoicelist/invoicelistbyid/:id', getInvoiceListbyid)
+
+
+// // module.exports = router
+
+
 // const express = require('express')
 // const router = express.Router()
 
-// const { getInvoices, getInvoice, createInvoice, deleteInvoice, updateInvoice, getInvoiceList, getInvoiceListbyid } = require('../controller/invoiceController')
+// const { getInvoices, getInvoice, createInvoice, deleteInvoice, updateInvoice, getInvoiceList, getInvoiceListbyid, getInvoiceListbyAccountid } = require('../controller/invoiceController')
 
 
 
@@ -12,25 +31,39 @@
 // router.patch('/invoice/:id', updateInvoice)
 // router.get('/invoice/invoicelist', getInvoiceList)
 // router.get('/invoice/invoicelist/invoicelistbyid/:id', getInvoiceListbyid)
+// router.get('/invoice/invoicelistby/accountid/:id', getInvoiceListbyAccountid); 
 
+// module.exports = router
+
+
+
+// const express = require('express')
+// const router = express.Router()
+
+// const { getInvoices, getInvoice, createInvoice, deleteInvoice, updateInvoice, getInvoiceList, getInvoiceListbyid } = require('../controller/invoiceController')
+
+// router.get('/invoice', getInvoices)
+// router.get('/invoice/:id', getInvoice)
+// router.post('/invoice', createInvoice)
+// router.delete('/invoice/:id', deleteInvoice)
+// router.patch('/invoice/:id', updateInvoice)
+// router.get('/invoice/invoicelist', getInvoiceList)
+// router.get('/invoice/invoicelist/invoicelistbyid/:id', getInvoiceListbyid)
 
 // module.exports = router
 
+const express = require("express");
+const router = express.Router();
 
-const express = require('express')
-const router = express.Router()
+const { getInvoices, getInvoice, createInvoice, deleteInvoice, updateInvoice, getInvoiceList, getInvoiceListbyid, getInvoiceListbyAccountid } = require("../controller/invoiceController");
 
-const { getInvoices, getInvoice, createInvoice, deleteInvoice, updateInvoice, getInvoiceList, getInvoiceListbyid, getInvoiceListbyAccountid } = require('../controller/invoiceController')
+router.get("/invoice", getInvoices);
+router.get("/invoice/:id", getInvoice);
+router.post("/invoice", createInvoice);
+router.delete("/invoice/:id", deleteInvoice);
+router.patch("/invoice/:id", updateInvoice);
+router.get("/invoice/invoicelist", getInvoiceList);
+router.get("/invoice/invoicelist/invoicelistbyid/:id", getInvoiceListbyid);
+router.get("/invoice/invoicelistby/accountid/:id", getInvoiceListbyAccountid);
 
-
-
-router.get('/invoice', getInvoices)
-router.get('/invoice/:id', getInvoice)
-router.post('/invoice', createInvoice)
-router.delete('/invoice/:id', deleteInvoice)
-router.patch('/invoice/:id', updateInvoice)
-router.get('/invoice/invoicelist', getInvoiceList)
-router.get('/invoice/invoicelist/invoicelistbyid/:id', getInvoiceListbyid)
-router.get('/invoice/invoicelistby/accountid/:id', getInvoiceListbyAccountid); 
-
-module.exports = router
+module.exports = router;
